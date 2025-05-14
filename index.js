17bf3a838ec54f037154a1cbce2fd07880c0afa9
DrorSBIsrael <Dror@sbparking.co.il>
2025-05-14 17:48:24 +0300

    Switch to SSL port 465 for SMTP

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 const transporter = nodemailer.createTransport({
   host: 'smtp.012.net.il',
   port: 465,
-  secure: SSL,
+  secure: true, // ? çåáä ìÎSSL
   auth: {
     user: 'Report@sbparking.co.il',
     pass: 'o51W38D5',
